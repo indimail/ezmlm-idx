76fff89df13f049fcbb96c7ed053f741b0d35857
Manvendra Bhangui <mbhangui@gmail.com>
2020-07-05 14:07:21 +0530

    use flags from the build system (rpm/debian)

--- ezmlm-idx-7.2.2/make-compile.sh
+++ ezmlm-idx-7.2.2/make-compile.sh
@@ -1,3 +1,12 @@
 echo 'source=$1; shift'
 echo 'base=`echo "$source" | sed -e s:\\\\.c$::`'
-echo exec "$CC" '-I. -o "$base".o -c "$source" ${1+"$@"}'
+set $CC
+cc_arg=$1
+shift
+echo "if [ -n \"\$CC\" ] ; then"
+echo "  " echo \""\$CC" \$CFLAGS $* '-I. -o "$base".o -c "$source" ${1+"$@"}'\"
+echo "  " exec   "\$CC" \$CFLAGS $* '-I. -o "$base".o -c "$source" ${1+"$@"}'
+echo "else"
+echo "  " echo \""$cc_arg" \$CFLAGS $* '-I. -o "$base".o -c "$source" ${1+"$@"}'\"
+echo "  " exec   "$cc_arg" \$CFLAGS $* '-I. -o "$base".o -c "$source" ${1+"$@"}'
+echo "fi"
--- ezmlm-idx-7.2.2/make-libcompile.sh
+++ ezmlm-idx-7.2.2/make-libcompile.sh
@@ -1,3 +1,12 @@
 echo 'source=$1; shift'
 echo 'base=`echo "$source" | sed -e s:\\\\.c$::`'
-echo exec "$CC $CCLO" '-I. -o "$base".lo -c "$source" ${1+"$@"}'
+set $CC
+cc_args=$1
+shift
+echo "if [ -n \"\$CC\" ] ; then"
+echo "  " echo \""\$CC" \$CFLAGS "$CCLO" $* '-I. -o "$base".lo -c "$source" ${1+"$@"}'\"
+echo "  " exec   "\$CC" \$CFLAGS "$CCLO" $* '-I. -o "$base".lo -c "$source" ${1+"$@"}'
+echo "else"
+echo "  " echo \""$cc_args" \$CFLAGS "$CCLO" $* '-I. -o "$base".lo -c "$source" ${1+"$@"}'\"
+echo "  " exec   "$cc_args" \$CFLAGS "$CCLO" $* '-I. -o "$base".lo -c "$source" ${1+"$@"}'
+echo "fi"
