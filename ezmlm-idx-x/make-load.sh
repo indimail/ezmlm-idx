219ffca7f80e0d6a9c97b5919b5596298f0c9886
Manvendra Bhangui <mbhangui@gmail.com>
2021-03-12 22:41:19 +0530

    updated build system for Linux, Darwin & FreeBSD

--- ezmlm-idx-x/make-load.sh
+++ ezmlm-idx-x/make-load.sh
@@ -2,19 +2,10 @@ echo 'main="$1"; shift'
 set $LD
 ld_args=$1
 shift
-if [ -d /usr/lib64/mysql ] ; then
-	mysql_libdir=-L/usr/lib64/mysql
-elif [ -d /usr/local/mysql ] ; then
-	mysql_libdir=-L/usr/local/lib/mysql
-elif [ -d /opt/local/mysql ] ; then
-	mysql_libdir=-L/opt/local/lib/mysql
-else
-	mysql_libdir=""
-fi
 echo "if [ -n \"\$CC\" ] ; then"
-echo "  " echo \""\$CC" \$LDFLAGS $* $mysql_libdir '-o "$main" "$main".o ${1+"$@"}'\"
-echo "  " exec   "\$CC" \$LDFLAGS $* $mysql_libdir '-o "$main" "$main".o ${1+"$@"}'
+echo "  " echo \""\$CC" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'\"
+echo "  " exec   "\$CC" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'
 echo "else"
-echo "  " echo \""$ld_args" \$LDFLAGS $* $mysql_libdir '-o "$main" "$main".o -L. ${1+"$@"}'\"
-echo "  " exec   "$ld_args" \$LDFLAGS $* $mysql_libdir '-o "$main" "$main".o -L. ${1+"$@"}'
+echo "  " echo \""$ld_args" \$LDFLAGS $* '-o "$main" "$main".o -L. ${1+"$@"}'\"
+echo "  " exec   "$ld_args" \$LDFLAGS $* '-o "$main" "$main".o -L. ${1+"$@"}'
 echo "fi"
