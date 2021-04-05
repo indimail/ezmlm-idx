c2abab86af6c060d562373f24396f970d85f4d09
Manvendra Bhangui <mbhangui@gmail.com>
2021-04-05 21:33:02 +0530

    make ezmlm-idx Arch Linux ready
    1. Create PKGBUILD
    2. modify prepare_obs to archive PKGBUILD
    3. New script create_archpkg to create arch linux packages

--- ezmlm-idx-x/fill-template.sh
+++ ezmlm-idx-x/fill-template.sh
@@ -1,4 +1,6 @@
 version=$(sed -e 's/^.*-//' -e q VERSION )
 release=$(cat conf-release)
+email=$(cat conf-email)
 sed -e "s/@version@/$version/" \
+	-e "s/@email@/$email/" \
 	-e "s/@release@/$release/"
