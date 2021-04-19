fc6b318daed24d4d4f7839c7d2c5c5f26a35b28f
Manvendra Bhangui <mbhangui@gmail.com>
2021-04-19 13:06:39 +0530

    replace prefix

--- ezmlm-idx-x/fill-template.sh
+++ ezmlm-idx-x/fill-template.sh
@@ -1,6 +1,8 @@
-version=$(sed -e 's/^.*-//' -e q VERSION )
+version=$(cat conf-version)
 release=$(cat conf-release)
+prefix=$(head -1 conf-prefix)
 email=$(cat conf-email)
 sed -e "s/@version@/$version/" \
 	-e "s/@email@/$email/" \
+	-e "s{@prefix@{$prefix{" \
 	-e "s/@release@/$release/"
